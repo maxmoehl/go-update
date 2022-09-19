cef10b7cee8128293b89e0bcb125cf45ad0cf33f
Maximilian Moehl <maximilian@moehl.eu>
2022-09-19 08:31:13 +0200

    feat: use go proxy

--- main.go
+++ main.go
@@ -1,22 +1,29 @@
 package main
 
 import (
-	"context"
 	"debug/buildinfo"
+	"encoding/json"
 	"fmt"
 	"io/fs"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
+)
 
-	"github.com/google/go-github/v45/github"
-	"golang.org/x/mod/semver"
-	"golang.org/x/oauth2"
+const (
+	updateCmd       = "update"
+	listCmd         = "list"
+	goBinEnv        = "GOBIN"
+	goPathEnv       = "GOPATH"
+	goProxyEnv      = "GOPROXY"
+	goMinVersionEnv = "GOMINVERSION"
+	homeEnv         = "HOME"
 )
 
 var (
-	client *github.Client
+	client = http.DefaultClient
 
 	printWarnings = false
 
@@ -29,56 +36,168 @@ var (
 	// of it has been present with go1.17, and it might work with go1.17
 	// binaries as well.
 	minGoVersion = "go1.18"
+
+	// goProxies contains the parsed list of the GOPROXY environment variable.
+	// It honors the definition at https://go.dev/ref/mod#environment-variables
+	goProxies = []string{"https://proxy.golang.org", "direct"}
+
+	goBin string
 )
 
+type info struct {
+	Version string
+	Time    time.Time
+}
+
+type binary struct {
+	name             string
+	pkg              string
+	installPath      string
+	installedVersion string
+	targetVersion    string
+	args             []string
+	env              []string
+}
+
+func (b binary) needsBump() bool {
+	return b.installedVersion != b.targetVersion
+}
+
+func (b binary) versionBump() string {
+	if b.needsBump() {
+		return fmt.Sprintf("%s -> %s", b.installedVersion, b.targetVersion)
+	} else {
+		return b.installedVersion
+	}
+}
+
+func (b binary) updateCmd() string {
+	return fmt.Sprintf("go install %s@%s", b.installPath, b.targetVersion)
+}
+
+type binaries []binary
+
+func (bs binaries) String() string {
+	packageColumnLen := 0
+	versionColumnLen := 0
+	for _, b := range bs {
+		if len(b.pkg) > packageColumnLen {
+			packageColumnLen = len(b.pkg)
+		}
+		version := b.versionBump()
+		if len(version) > versionColumnLen {
+			versionColumnLen = len(version)
+		}
+	}
+
+	builder := strings.Builder{}
+	for _, b := range bs {
+		builder.WriteString(b.pkg)
+		builder.WriteString(strings.Repeat(" ", packageColumnLen-len(b.pkg)+1))
+		version := b.versionBump()
+		builder.WriteString(version)
+		if b.needsBump() {
+			builder.WriteString(strings.Repeat(" ", versionColumnLen-len(version)+1))
+			builder.WriteString(b.updateCmd())
+		}
+		builder.WriteRune('\n')
+	}
+
+	return builder.String()
+}
+
 func init() {
+	var err error
+	defer func() {
+		if err != nil {
+			fmt.Printf("error: %s\n", err.Error())
+			os.Exit(1) // exit code 1: error during init
+		}
+	}()
+
 	_, printWarnings = os.LookupEnv("PRINT_WARNINGS")
-	customMinGoVersion, exists := os.LookupEnv("MIN_GO_VERSION")
-	if exists {
+	customMinGoVersion, ok := os.LookupEnv(goMinVersionEnv)
+	if ok {
 		minGoVersion = customMinGoVersion
 	}
 
-	token := os.Getenv("GITHUB_TOKEN")
-	if token == "" {
-		panic("please set GITHUB_TOKEN")
+	goBin = os.Getenv(goBinEnv)
+	if goBin == "" && os.Getenv(goPathEnv) != "" {
+		goBin = filepath.Join(os.Getenv(goPathEnv), "bin")
+	} else if goBin == "" && os.Getenv(homeEnv) != "" {
+		goBin = filepath.Join(os.Getenv(homeEnv), "go", "bin")
+	} else if goBin == "" {
+		err = fmt.Errorf("unable to determine GOBIN: $GOBIN, $GOPATH and $HOME are not set")
+		return
 	}
 
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: token},
-	)
-	tc := oauth2.NewClient(context.Background(), ts)
-	client = github.NewClient(tc)
+	fileInfo, err := os.Stat(goBin)
+	if err != nil {
+		err = fmt.Errorf("unable to stat GOBIN (%s): %s", goBin, err.Error())
+		return
+	}
+	if !fileInfo.IsDir() {
+		err = fmt.Errorf("error: init: GOBIN (%s) is not a directory", goBin)
+		return
+	}
+
+	proxy, ok := os.LookupEnv(goProxyEnv)
+	if ok {
+		goProxies = strings.Split(proxy, ",")
+	}
 }
 
 func main() {
-	err := update()
-	if err != nil {
-		fmt.Printf("error: %s\n", err.Error())
+	var err error
+	defer func() {
+		if err != nil {
+			fmt.Printf("error: %s\n", err.Error())
+			os.Exit(2) // exit code 2: generic error during execution
+		}
+	}()
+
+	if len(os.Args) > 2 {
+		err = fmt.Errorf("error: only one argument can be provided")
+		return
+	}
+
+	fmt.Printf("using %s\n", goBin)
+
+	if len(os.Args) < 2 || os.Args[1] == updateCmd {
+		err = update(goBin)
+	} else if os.Args[1] == listCmd {
+		bs, err := list(goBin)
+		if err != nil {
+			return
+		}
+		fmt.Printf(bs.String())
+	} else {
+		err = fmt.Errorf("unknown command '%s'", os.Args[1])
 	}
 }
 
-func update() error {
-	binDir, err := goBin()
+func update(binDir string) error {
+	bs, err := list(binDir)
 	if err != nil {
 		return fmt.Errorf("udpate: %w", err)
 	}
 
-	fileInfo, err := os.Stat(binDir)
-	if err != nil {
-		return fmt.Errorf("udpate: %w", err)
+	for _, b := range bs {
+		if b.needsBump() {
+			fmt.Println(b.updateCmd())
+		}
 	}
 
-	fmt.Printf("using %s\n", binDir)
+	return nil
+}
 
-	if !fileInfo.IsDir() {
-		return fmt.Errorf("GOBIN (%s) is not a directory", binDir)
-	}
-
+func list(binDir string) (binaries, error) {
 	entries, err := fs.ReadDir(os.DirFS(binDir), ".")
 	if err != nil {
-		return fmt.Errorf("update: %w", err)
+		return nil, fmt.Errorf("list: %w", err)
 	}
 
+	var bs binaries
 	for _, entry := range entries {
 		if entry.IsDir() {
 			warn(fmt.Sprintf("skipping directory '%s'", entry.Name()))
@@ -87,7 +206,7 @@ func update() error {
 
 		fileInfo, err := entry.Info()
 		if err != nil {
-			return fmt.Errorf("update: %w", err)
+			return nil, fmt.Errorf("list: %w", err)
 		}
 
 		if !executable(fileInfo.Mode()) {
@@ -95,97 +214,65 @@ func update() error {
 			continue
 		}
 
-		err = updateBin(filepath.Join(binDir, entry.Name()))
-		if err != nil && printWarnings {
+		b, err := inspectBinary(filepath.Join(binDir, entry.Name()))
+		if err != nil {
 			warn(fmt.Sprintf("skipping '%s': %s", entry.Name(), err.Error()))
+			continue
 		}
+
+		bs = append(bs, b)
 	}
 
-	return nil
+	return bs, nil
 }
 
-func updateBin(path string) error {
+func inspectBinary(path string) (binary, error) {
 	info, err := buildinfo.ReadFile(path)
 	if err != nil {
-		return fmt.Errorf("udpate binary: unable to read buildinfo: %w", err)
+		return binary{}, fmt.Errorf("udpate binary: unable to read buildinfo: %w", err)
 	}
 	if info.GoVersion < minGoVersion {
-		return fmt.Errorf("udpate binary: go version too old to update (%s)", info.GoVersion)
-	}
-	if !strings.HasPrefix(info.Main.Path, "github.com/") {
-		return fmt.Errorf("udpate binary: only packages from github.com are supported as of now")
+		return binary{}, fmt.Errorf("udpate binary: go version too old to update (%s)", info.GoVersion)
 	}
 
-	// 0: github.com; 1: owner; 2: repo name; 3+: path
-	repositoryParts := strings.Split(info.Main.Path, "/")
-
-	tags, res, err := client.Repositories.ListTags(context.Background(), repositoryParts[1], repositoryParts[2], nil)
+	targetVersion, err := latest(info.Main.Path)
 	if err != nil {
-		return fmt.Errorf("update binary: %w", err)
+		return binary{}, fmt.Errorf("update binary: %w", err)
 	}
 
-	if res.StatusCode != http.StatusOK {
-		return fmt.Errorf("update binary: http request failed: %s", res.Status)
-	}
-
-	// we consider everything that can't be parsed or is installed as v0.0.0 as invalid
-	// TODO: for modules that don't use tags, semver and modules we need to figure something out
-	if !semver.IsValid(info.Main.Version) || semver.Compare(info.Main.Version, "v0.0.0") == 0 {
-		return fmt.Errorf("udpate binary: invalid version installed: %s", info.Main.Version)
-	}
-
-	targetVersion := info.Main.Version
-	for _, tag := range tags {
-		if semver.Compare(targetVersion, *tag.Name) < 0 {
-			targetVersion = *tag.Name
-		}
-	}
-
-	if targetVersion == info.Main.Version {
-		fmt.Printf("%s\tup to date: %s\n", info.Main.Path, info.Main.Version)
-		return nil
-	}
-
-	// TODO: env?
-
-	args := []string{"go", "install"}
+	var args []string
 	for _, setting := range info.Settings {
 		if in(setting.Key, preserveSettings) {
 			args = append(args, fmt.Sprintf("%s=%s", setting.Key, setting.Value))
 		}
 	}
-	args = append(args, fmt.Sprintf("%s@%s", info.Path, targetVersion))
 
-	b := strings.Builder{}
-	b.WriteString(info.Main.Path)
-	b.WriteRune('\t')
-	b.WriteString(info.Main.Version)
-	b.WriteString(" -> ")
-	b.WriteString(targetVersion)
-	b.WriteRune('\t')
-	b.WriteString(strings.Join(args, " "))
-
-	fmt.Println(b.String())
-	return nil
+	return binary{
+		name:             filepath.Base(path),
+		pkg:              info.Main.Path,
+		installPath:      info.Path,
+		installedVersion: info.Main.Version,
+		targetVersion:    targetVersion,
+		args:             args,
+		env:              nil,
+	}, nil
 }
 
-func goBin() (string, error) {
-	bin := os.Getenv("GOBIN")
-	if bin != "" {
-		return bin, nil
+// latest returns the latest version according to the GOPROXY.
+func latest(module string) (string, error) {
+	latestUrl := fmt.Sprintf("https://proxy.golang.org/%s/@latest", module)
+	res, err := client.Get(latestUrl)
+	if err != nil {
+		return "", fmt.Errorf("latest: %w", err)
 	}
 
-	gopath := os.Getenv("GOPATH")
-	if gopath != "" {
-		return filepath.Join(gopath, "bin"), nil
+	var latestVersion info
+	err = json.NewDecoder(res.Body).Decode(&latestVersion)
+	if err != nil {
+		return "", fmt.Errorf("latest: %w", err)
 	}
 
-	home := os.Getenv("HOME")
-	if home != "" {
-		return filepath.Join(home, "go", "bin"), nil
-	}
-
-	return "", fmt.Errorf("go bin: unable to determine GOBIN: $GOBIN, $GOPATH and $HOME are not set")
+	return latestVersion.Version, nil
 }
 
 func warn(msg string) {
